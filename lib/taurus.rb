bc5ea9da23b400bbcd7dd87021452f3b0d15d471
Murilo Roque <muriloengqui@gmail.com>
2020-08-31 18:16:16 -0300

    add taurus spider

--- lib/taurus.rb
+++ lib/taurus.rb
@@ -6,8 +6,12 @@ class TaurusSpider < Kimurai::Base
 
   def parse(response, url:, data: {})
 
-    urls = response.css('#s5_component_call_wrap_inner > div.blog.apartments > div.items-row.cols-3.row-0.row-fluid.clearfix > div:nth-child(1) > div > div > div > a').map { |elem| elem = elem[:href] }.compact
+    urls = response.css('div.blog.apartments div.inner a').map { |elem| elem = elem[:href] }.compact.map { |x| 'https://www.taurus.dk/' + x }
     in_parallel(:parse_listing, urls, threads: 3)
+
+    if next_page = response.at_css("#s5_component_call_wrap_inner > div.blog.apartments > div.pagination > div > a:nth-child(8)")
+      request_to :parse, url: absolute_url(next_page[:href], base: url)
+    end
 
   end
 
@@ -16,22 +20,24 @@ class TaurusSpider < Kimurai::Base
     begin
 
       item = { external_source: self.class.to_s, external_link: url }
-      # item[:title] = response.css('#main > div.houseInfo.pure-u-1.pure-u-sm-18-24.pure-u-md-18-24 > h2').text
-      # item[:description] = response.css('#main > div.houseInfo.pure-u-1.pure-u-sm-18-24.pure-u-md-18-24 > div.section.info').text
-      # left = response.css('#main > div.houseInfo.pure-u-1.pure-u-sm-18-24.pure-u-md-18-24 > div.houseDetails.pure-g > div.column1.pure-u-1.pure-u-sm-1.pure-u-md-2-3 > div.section.lastSection.left > table > tbody')
-      # right = response.css('#main > div.houseInfo.pure-u-1.pure-u-sm-18-24.pure-u-md-18-24 > div.houseDetails.pure-g > div.column1.pure-u-1.pure-u-sm-1.pure-u-md-2-3 > div.column2.pure-u-1.pure-u-sm-1.pure-u-md-1-3 > table > tbody')
-      # item[:city] = left.css('tr:first-child td:last-child').text
-      # item[:property_type] = left.css('tr:nth-child(2) td:last-child').text
-      # item[:pet_allowed] = left.css('tr:nth-child(5) td:last-child').text =~ /Ja/i ? true : false
-      # item[:square_meters] = right.css('tr:nth-child(5) td:last-child').text.gsub('m2', '').strip
-      # item[:room_count] = right.css('tr:nth-child(6) td:last-child').text
-      # item[:rent] = right.css('tr:nth-child(1) td:last-child').text.scan(/\d/).join('')
-      # item[:deposit] = right.css('tr:nth-child(2) td:last-child').text.scan(/\d/).join('')
-      # item[:heating_cost] = right.css('tr:nth-child(3) td:last-child').text.scan(/\d/).join('')
-      # item[:images] = response.css('#main > div.houseInfo.pure-u-1.pure-u-sm-18-24.pure-u-md-18-24 > div.imageContainer.pure-u-1.pure-u-sm-1.pure-u-md-2-3 > img').pluck(:src).map { |x| 'http://logstorbo.dk' + x }
-      # item[:landlord_name] = 'Løgstør Boligforening'
-      # item[:landlord_phone] = '98673241'
-      # item[:landlord_email] = 'post@logstorbo.dk'
+      item[:title] = response.css('#leje_layout > div.description-box > h1').text
+      item[:description] = response.css('#leje_layout > div.description-box > p').text
+      table = response.css('#leje_layout > div.fakta-box.s5_float_left > div.module_round_box > div > div > div:nth-child(2) > table')
+      item[:zipcode] = table.css('/tr[contains("Postnummer")]').text.scan(/\d/).join('')
+      address = table.css('/tr[contains("Vejnavn")]').text.gsub("Vejnavn",'') + ', ' + table.css('/tr[contains("Husnummer")]').text.scan(/\d/).join('') + ', ' + item[:zipcode]
+      item[:address] = address.squish
+      item[:property_type] = table.css('/tr[contains("Boligtype")]').text.gsub("Boligtype",'').squish
+      item[:pet_allowed] = item[:square_meters] = table.css('/tr[contains("Husdyr tilladt")]').text.gsub("Boligareal (m2)",'').squish =~ /Ja/i ? true : false
+      item[:square_meters] = table.css('/tr[contains("Boligareal (m2)")]').text.gsub("Boligareal (m2)",'').squish
+      item[:room_count] = table.css('/tr[contains("Værelser")]').text.gsub("Værelser",'').scan(/\d/).join('')
+      item[:available_date] = table.css('/tr[contains("Overtagelsesdato")]').text.gsub("Overtagelsesdato",'').squish
+      item[:rent] = table.css('/tr[contains("Husleje (Mdl.)")]').text.scan(/\d/).join('')
+      item[:deposit] = table.css('/tr[contains("Depositum")]').text.scan(/\d/).join('')
+      item[:prepaid_rent] = table.css('/tr[contains("Forudbetalt leje")]').text.scan(/\d/).join('')
+      item[:floor] = table.css('/tr[contains("Etage")]').text.scan(/\d/).join('')
+      item[:landlord_name] = 'Taurus Property'
+      item[:landlord_phone] = '86122020'
+      item[:landlord_email] = 'pjj@taurus.dk'
       save_to "results.json", item, format: :pretty_json
     rescue => e
       logger.error url
